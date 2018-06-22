b4bb059fc3d7219a9f1cb91fc931168ebf31b8d1
LENOVO <LENOVO@LENOVO-PC>
2018-06-22 22:08:42 +0530

    db

---
+++ src/main/java/com/javamaster/connection/DbConnection.java
@@ -0,0 +1,30 @@
+package com.javamaster.connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+
+public class DbConnection 
+{
+private static Connection conn;
+
+
+static
+	{
+	try{
+	Class.forName("com.mysql.jdbc.Driver");
+	conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/chat","root","");
+	}catch(Exception e)
+	{
+		e.printStackTrace();
+	}
+	}
+public static Connection getConnection()
+{
+return conn;	
+}
+
+
+}
+
+
